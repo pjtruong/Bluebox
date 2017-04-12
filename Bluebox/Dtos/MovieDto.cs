5659a43224c0ea8300befc4a7a3005ab59e17a6e
pjtruong <hp5340@gmail.com>
2017-04-12 03:20:23 -0700

    added movies web apis for crud operations and jquery front end functions

---
+++ Bluebox/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Bluebox.Models;
+
+namespace Bluebox.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int StockNum { get; set; }
+    }
+}
